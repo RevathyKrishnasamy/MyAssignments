ef2cfff053a9570174bd0a9cb459cd86ad002329
RevathyKrishnasamy <124457298+RevathyKrishnasamy@users.noreply.github.com>
2023-02-04 23:45:52 +0530

    Add files via upload
    Added Mobile and Two Wheeler files

---
+++ day1/Mobile.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName="Samsung";
+		char mobileLogo='s';
+		short noOfMobilePiece=100;
+		int modelNumber=2345;
+		long mobileImeiNumber=123456789;
+		float mobilePrice=18900.50f;
+		boolean isDamaged=false;
+		
+		System.out.println("mobileBrandName : "+mobileBrandName);
+		System.out.println("mobileLogo : "+mobileLogo);
+		System.out.println(noOfMobilePiece);
+		System.out.println(modelNumber);
+		System.out.println(mobileImeiNumber);
+		System.out.println(mobilePrice);
+		System.out.println(isDamaged);
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels= 2;
+		short noOfMirrors=2;
+		long chassisNumber=678902647;
+		boolean isPunctured=true;
+		String bikeName="Hero";
+		double runningKM=250;
+		
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKM);
+					
+	}
+
+}
