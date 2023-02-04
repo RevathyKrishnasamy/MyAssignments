f250337ec2405b082581e3dbf589c3b1a3d9270b
RevathyKrishnasamy <124457298+RevathyKrishnasamy@users.noreply.github.com>
2023-02-04 22:53:19 +0530

    Add files via upload
    Added Day 1 (Feb 4) assignments

---
+++ day1/DataType.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class DataType {
+	public static void main(String[] args) {
+		int inputOne= 24;
+		boolean a=true;
+		String name= "hello my name is Reavthy";
+System.out.println(name);		
+System.out.println(inputOne);
+System.out.println(a);
+
+	}
+
+}
